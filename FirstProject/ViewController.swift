2ca44c41b0246ae06eb6b705cc3dff8aa2fa65e0
TeklaMach <macharashvilitekla@gmail.com>
2023-10-23 07:18:05 +0400

    adding the first project

--- FirstProject/ViewController.swift
+++ FirstProject/ViewController.swift
@@ -4,16 +4,59 @@
 //
 //  Created by Tekla Matcharashvili on 22.10.23.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mathActions: UILabel!
+    @IBOutlet weak var answers: UILabel!
+    @IBOutlet weak var actionButton: UIButton!
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var controler: UISwitch!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateMathActionLabel()
     }
 
+    @IBAction func switchStateChanged(_ sender: UISwitch) {
+        updateMathActionLabel()
+    }
 
+    @IBAction func calculateButtonTapped(_ sender: UIButton) {
+        calculateResult()
+    }
+
+    func updateMathActionLabel() {
+        if controler.isOn {
+            mathActions.text = "GCD"
+        } else {
+            mathActions.text = "LCM"
+        }
+    }
+
+    func calculateResult() {
+        guard let number1 = Double(textField1.text ?? ""),
+              let number2 = Double(textField2.text ?? "") else {
+            answers.text = "Invalid input"
+            return
+        }
+
+        if controler.isOn { 
+            let result = greatestCommonDivisor(a: Int(number1), b: Int(number2))
+            answers.text = "Result: \(result)"
+        } else {
+            let result = leastCommonMultiple(a: Int(number1), b: Int(number2))
+            answers.text = "Result: \(result)"
+        }
+    }
+
+    func greatestCommonDivisor(a: Int, b: Int) -> Int {
+        return b == 0 ? a : greatestCommonDivisor(a: b, b: a % b)
+    }
+
+    func leastCommonMultiple(a: Int, b: Int) -> Int {
+        return a * b / greatestCommonDivisor(a: a, b: b)
+    }
 }
-
